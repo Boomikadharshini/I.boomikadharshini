7b91efd115da4ead19632d7a7cf6a566447e9e6a
Boomikadharshini <dharshinitharika78@gmail.com>
2024-08-29 16:14:32 +0530

    Add files via upload

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_fahrenheit = new System.Windows.Forms.TextBox();
+            this.txt_celsius = new System.Windows.Forms.TextBox();
+            this.txt_kelvin = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(63, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Fahrenheit";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(60, 166);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Celsius";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(63, 281);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Kelvin";
+            // 
+            // txt_fahrenheit
+            // 
+            this.txt_fahrenheit.Location = new System.Drawing.Point(242, 57);
+            this.txt_fahrenheit.Name = "txt_fahrenheit";
+            this.txt_fahrenheit.Size = new System.Drawing.Size(100, 20);
+            this.txt_fahrenheit.TabIndex = 3;
+            this.txt_fahrenheit.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txt_celsius
+            // 
+            this.txt_celsius.Location = new System.Drawing.Point(242, 166);
+            this.txt_celsius.Name = "txt_celsius";
+            this.txt_celsius.Size = new System.Drawing.Size(100, 20);
+            this.txt_celsius.TabIndex = 4;
+            this.txt_celsius.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // txt_kelvin
+            // 
+            this.txt_kelvin.Location = new System.Drawing.Point(242, 274);
+            this.txt_kelvin.Name = "txt_kelvin";
+            this.txt_kelvin.Size = new System.Drawing.Size(100, 20);
+            this.txt_kelvin.TabIndex = 5;
+            this.txt_kelvin.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(509, 54);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Go";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(509, 156);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Go";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(509, 271);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Go";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(823, 386);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txt_kelvin);
+            this.Controls.Add(this.txt_celsius);
+            this.Controls.Add(this.txt_fahrenheit);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "c";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_fahrenheit;
+        private System.Windows.Forms.TextBox txt_celsius;
+        private System.Windows.Forms.TextBox txt_kelvin;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+           // convertFromFahrenheit();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            convertFromFahrenheit();
+        }
+
+        private void convertFromFahrenheit()
+        {
+            float f = 0;
+            try
+            {
+                f = float.Parse(txt_fahrenheit.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Enter numbers only");
+            }
+            float c = 5f / 9f * (f - 32f);
+            float k = c - 273;
+
+            txt_celsius.Text=c.ToString();
+            txt_kelvin.Text=k.ToString();
+        }
+
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            //convertFromCelsius();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            convertFromCelsius();
+        }
+
+        private void convertFromCelsius()
+        {
+            float c = 0;
+            try
+            {
+                c = float.Parse(txt_celsius.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Enter numbers only");
+            }
+            float f = c * (9 / 5) + 32;
+            float k = c + 273; 
+
+            txt_fahrenheit.Text = f.ToString();
+            txt_kelvin.Text = k.ToString();
+        }
+
+
+
+
+
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            convertFromkelvin();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            convertFromkelvin();
+        }
+
+
+        private void convertFromkelvin()
+        {
+            float k = 0;
+            try
+            {
+                k = float.Parse(txt_kelvin.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Enter numbers only");
+            }
+            float f = (k - 273) * (9 / 5) + 32;
+            float c = k - 27;
+
+            txt_fahrenheit.Text = f.ToString();
+            txt_celsius.Text = c.ToString();
+        }
+
+
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
